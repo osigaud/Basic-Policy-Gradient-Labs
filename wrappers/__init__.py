47278463e9fbc2fbfb8e2f08191eaf870e1d0aeb
osigaud <Olivier.Sigaud@upmc.fr>
2020-08-14 11:13:21 +0200

    adding comments, debugging and clarifying

--- wrappers/__init__.py
+++ wrappers/__init__.py
@@ -2,4 +2,5 @@ from gym import error
 from wrappers.feature_inverter import FeatureInverter
 from wrappers.action_vector_adapter import ActionVectorAdapter
 from wrappers.binary_shifter import BinaryShifter
+from wrappers.binary_shifter_discrete import BinaryShifterDiscrete
 from wrappers.perf_writer import PerfWriter
