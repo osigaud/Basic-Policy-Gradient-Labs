079e90b2dd542f258dddcda3a38c9a67ff006aa8
Florian Cormée <florian.cormee@gmail.com>
2020-11-22 11:36:27 +0100

    Final cleanup

--- critics/q_net.py
+++ critics/q_net.py
@@ -7,28 +7,71 @@ from policies import GenericNet
 
 
 class QNet(GenericNet):
-    def __init__(self, learning_rate, gamma=0.98, tau=0.01):
+    def __init__(self, learning_rate, tau=0.01, state_size=3, action_size=1, hidden_layer1=128, hidden_layer2=32,
+                 output_size=1):
+        """Build a double Q network.
+
+        Args:
+            learning_rate (float): The learning rate.
+            tau (float): The update rate between 0 and 1 used to update the target network.
+            state_size (int): The number of elements in a single state vector.
+            action_size (int): The number of elements in a single action vector.
+            hidden_layer1 (int): The number of neurons of the first hidden layer (should be even).
+            hidden_layer2 (int): The number of neurons of the second hidden layer.
+            output_size (int): The number of outputs.
+        """
         super(QNet, self).__init__()
-        self.gamma = gamma
         self.tau = tau
 
-        self.fc_s = nn.Linear(3, 64)
-        self.fc_a = nn.Linear(1, 64)
-        self.fc_cat = nn.Linear(128, 32)
-        self.fc_out = nn.Linear(32, 1)
+        half_hidden_layer1 = hidden_layer1 // 2
+        self.fc_s = nn.Linear(state_size, half_hidden_layer1)
+        self.fc_a = nn.Linear(action_size, half_hidden_layer1)
+        self.fc_cat = nn.Linear(2 * half_hidden_layer1, hidden_layer2)
+        self.fc_out = nn.Linear(hidden_layer2, output_size)
         self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
 
         self.losses = None
 
-    def forward(self, x, a):
-        h1 = F.relu(self.fc_s(x))
-        h2 = F.relu(self.fc_a(a))
+    def forward(self, state, action):
+        """Forward the state and to action through the network.
+
+        Args:
+            state (torch.Tensor): The column tensor of state(s).
+            action (torch.Tensor): The column tensor of action(s).
+
+        Returns:
+            torch.Tensor: The critic value at a state action pair.
+        """
+        h1 = F.relu(self.fc_s(state))
+        h2 = F.relu(self.fc_a(action))
         cat = torch.cat([h1, h2], dim=1)
         q = F.relu(self.fc_cat(cat))
         q = self.fc_out(q)
         return q
 
+    def evaluate(self, state, action):
+        """Return the critic value at a state action pair, as a numpy structure
+
+        Args:
+            state (numpy.ndarray): the given state
+            action (numpy.ndarray): the given action
+
+        Returns:
+             numpy.ndarray: The value.
+        """
+        state = torch.from_numpy(state).float().reshape(1, -1)
+        action = torch.from_numpy(action).float().reshape(1, -1)
+        x = self.forward(state, action)
+        return x.data.numpy()
+
     def train_net(self, target, mini_batch):
+        """Train the network.
+
+        Args:
+            target (torch.Tensor): The target value.
+            mini_batch (torch.Tensor): The mini batch of data containing the state, the action, the reward,
+            the next state and 1 - done in each row.
+        """
         s, a, r, s_prime, done = mini_batch
         loss = F.smooth_l1_loss(self.forward(s, a), target)
         self.losses = loss.data.numpy().astype(float).mean()
@@ -36,33 +79,11 @@ class QNet(GenericNet):
         loss.mean().backward()
         self.optimizer.step()
 
-    def soft_update(self, net_target):
-        for param_target, param in zip(net_target.parameters(), self.parameters()):
-            param_target.data.copy_(
-                param_target.data * (1.0 - self.tau) + param.data * self.tau)
+    def soft_update(self, target_network):
+        """Update the weights of the target network.
 
-    def evaluate(self, state, action):
+        Args:
+            target_network (critics.QNet): The target network to update.
         """
-        Return the critic value at a state action pair, as a numpy structure
-        :param state: the given state
-        :param action: the given action
-        :return: the value
-        """
-        state = torch.from_numpy(state).float().reshape(1, -1)
-        action = torch.from_numpy(action).float().reshape(1, -1)
-        x = self.forward(state, action)
-        return x.data.numpy()
-
-
-def calc_target(pi, q1, q2, mini_batch):
-    s, a, r, s_prime, done = mini_batch
-
-    with torch.no_grad():
-        a_prime, log_prob = pi(s_prime)
-        entropy = -pi.log_alpha.exp() * log_prob
-        q1_val, q2_val = q1(s_prime, a_prime), q2(s_prime, a_prime)
-        q1_q2 = torch.cat([q1_val, q2_val], dim=1)
-        min_q = torch.min(q1_q2, 1, keepdim=True)[0]
-        target = r + q1.gamma * done * (min_q + entropy)
-
-    return target
+        for param_target, param in zip(target_network.parameters(), self.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) + param.data * self.tau)
